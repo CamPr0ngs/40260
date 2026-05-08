4cf0607fd194b6846c70f3e6e57ac2dcb37f071f
CamPr0ngs <camilacrespozurita@gmail.com>
2026-05-08 11:21:11 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,56 +1,60 @@
-/* let b1 = document.getElementById("b1")
-let b2 = document.getElementById("b2")
-let b3 = document.getElementById("b3")
-let b4 = document.getElementById("b4")
-let b5 = document.getElementById("b5")
-let b6 = document.getElementById("b6")
-let b7 = document.getElementById("b7")
-let b8 = document.getElementById("b8")
-let b9 = document.getElementById("b9")
-let b0 = document.getElementById("b0")
- */
+let resultado = document.getElementById("resultado");
+let num1 = 0;
+let operacion = "";
 
-let resultado = document.getElementById("resultado")
+let botones = document.getElementsByTagName("button");
 
-let botones = document.getElementsByTagName("button")
 for (const key in botones) {
     if (Object.hasOwnProperty.call(botones, key)) {
-        const boton = botones[key];
+        const boton = botones[key];        
         if (boton.className != "operadores") {
-            boton.addEventListener("click", pintar)
+            boton.addEventListener("click", escribirNumero);
         } else {
-            boton.addEventListener("click", pintar2)
+            boton.addEventListener("click", manejarOperador);
         }
-        //console.log(boton)
-    }
-
-}
-
-function pintar(e) {
-    console.log(e.target.innerText)
-    resultado.value += e.target.innerText
-}
-
-let operadores = document.getElementsByClassName("operadores")
-for (const key in operadores) {
-    if (Object.prototype.hasOwnProperty.call(operadores, key)) {
-        const boton = operadores[key];
-        boton.addEventListener("click", pintar2)
     }
 }
 
-function pintar2(e) {
-    console.log(e.target.innerText)
-    if (e.target.innerText == "+")
-        suma();
+function escribirNumero(e) {    
+    resultado.value += e.target.innerText;
 }
 
-function suma() {
-    num1 = resultado.value
-    resultado.value = ""
+function manejarOperador(e) {
+    let valorBoton = e.target.innerText;
+
+    if (valorBoton === "CE") {        
+        resultado.value = "";
+        num1 = 0;
+        operacion = "";
+    } else if (valorBoton === "=") {        
+        ejecutarOperacion();
+    } else {        
+        num1 = parseFloat(resultado.value);
+        operacion = valorBoton;
+        resultado.value = ""; 
+    }
 }
 
-function igual() {
-    num2 = resultado.value
-    resultado.value = parseInt(num1) + parseInt(num2)
+function ejecutarOperacion() {
+    let num2 = parseFloat(resultado.value);
+    let total = 0;
+
+    switch (operacion) {
+        case "+":
+            total = num1 + num2;
+            break;
+        case "-":
+            total = num1 - num2;
+            break;
+        case "*":
+            total = num1 * num2;
+            break;
+        case "/":
+            total = num1 / num2;
+            break;
+    }
+    
+    resultado.value = total;    
+    num1 = total;
+    operacion = "";
 }
