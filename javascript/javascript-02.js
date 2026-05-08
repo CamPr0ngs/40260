f7d58319adfee7d0f4c3681ae6804ba210433ad1
CamPr0ngs <camilacrespozurita@gmail.com>
2026-05-08 11:23:14 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,3 +1,15 @@
+/* let b1 = document.getElementById("b1")
+let b2 = document.getElementById("b2")
+let b3 = document.getElementById("b3")
+let b4 = document.getElementById("b4")
+let b5 = document.getElementById("b5")
+let b6 = document.getElementById("b6")
+let b7 = document.getElementById("b7")
+let b8 = document.getElementById("b8")
+let b9 = document.getElementById("b9")
+let b0 = document.getElementById("b0")
+ */
+
 let resultado = document.getElementById("resultado");
 let num1 = 0;
 let operacion = "";
